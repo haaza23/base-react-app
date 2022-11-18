3fdfc5cdafbf02b0f228f45fb70290e2e5658ac5
Nico Hazan <nicohaza@gmail.com>
2022-11-18 15:10:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import LoginContainer from './containers/LoginContainer';
+
+const App: FunctionComponent = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
